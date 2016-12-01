8704d1e88ccc9fcd8c89fd46a5484b692489bab9
muhammadimam <frauhoujiro@gmail.com>
2016-12-01 09:01:23 +0700

    first commit

---
+++ app/app.js
@@ -0,0 +1,154 @@
+/*global angular*/
+angular
+  .module('CimerangFarm', ['ngAnimate', 'ngCookies', 'ngTouch',
+  'ngSanitize', 'ui.router', 'ngMaterial', 'nvd3', 'firebase'])
+
+  .config(function ($mdThemingProvider) {
+    'use strict';
+  
+    $mdThemingProvider
+      .theme('default')
+        .primaryPalette('amber', {
+          'default': '600'
+        })
+        .accentPalette('teal', {
+          'default': '500'
+        })
+        .warnPalette('defaultPrimary');
+
+    $mdThemingProvider.theme('dark', 'default')
+      .primaryPalette('defaultPrimary')
+      .dark();
+
+    $mdThemingProvider.theme('amber', 'default')
+      .primaryPalette('amber');
+
+    $mdThemingProvider.theme('custom', 'default')
+      .primaryPalette('defaultPrimary', {
+        'hue-1': '50'
+    });
+
+    $mdThemingProvider.definePalette('defaultPrimary', {
+      '50':  '#FFFFFF',
+      '100': 'rgb(255, 198, 197)',
+      '200': '#E75753',
+      '300': '#E75753',
+      '400': '#E75753',
+      '500': '#E75753',
+      '600': '#E75753',
+      '700': '#E75753',
+      '800': '#E75753',
+      '900': '#E75753',
+      'A100': '#E75753',
+      'A200': '#E75753',
+      'A400': '#E75753',
+      'A700': '#E75753'
+    });
+  })
+
+  .controller('MainController', [
+          'navService', '$mdSidenav', '$mdBottomSheet', '$log', '$q', '$state', '$mdToast',
+          MainController
+       ]);
+
+  function MainController(navService, $mdSidenav, $mdBottomSheet, $log, $q, $state, $mdToast) {
+    var vm = this;
+
+    vm.menuItems = [ ];
+    vm.selectItem = selectItem;
+    vm.toggleItemsList = toggleItemsList;
+    vm.showActions = showActions;
+    vm.title = $state.current.data.title;
+    vm.showSimpleToast = showSimpleToast;
+    vm.toggleRightSidebar = toggleRightSidebar;
+
+    navService
+      .loadAllItems()
+      .then(function(menuItems) {
+        vm.menuItems = [].concat(menuItems);
+      });
+
+    function toggleRightSidebar() {
+        $mdSidenav('right').toggle();
+    }
+
+    function toggleItemsList() {
+      var pending = $mdBottomSheet.hide() || $q.when(true);
+
+      pending.then(function(){
+        $mdSidenav('left').toggle();
+      });
+    }
+
+    function selectItem (item) {
+      vm.title = item.name;
+      vm.toggleItemsList();
+      vm.showSimpleToast(vm.title);
+    }
+
+    function showActions($event) {
+        $mdBottomSheet.show({
+          parent: angular.element(document.getElementById('content')),
+          templateUrl: 'app/views/partials/bottomSheet.html',
+          controller: [ '$mdBottomSheet', SheetController],
+          controllerAs: "vm",
+          bindToController : true,
+          targetEvent: $event
+        }).then(function(clickedItem) {
+          clickedItem && $log.debug( clickedItem.name + ' clicked!');
+        });
+
+        function SheetController( $mdBottomSheet ) {
+          var vm = this;
+
+          vm.actions = [
+            { name: 'Share', icon: 'share', url: 'https://twitter.com/intent/tweet?text=Angular%20Material%20Dashboard%20https://github.com/flatlogic/angular-material-dashboard%20via%20@flatlogicinc' },
+            { name: 'Star', icon: 'star', url: 'https://github.com/flatlogic/angular-material-dashboard/stargazers' }
+          ];
+
+          vm.performAction = function(action) {
+            $mdBottomSheet.hide(action);
+          };
+        }
+    }
+
+    function showSimpleToast(title) {
+      $mdToast.show(
+        $mdToast.simple()
+          .content(title)
+          .hideDelay(2000)
+          .position('bottom right')
+      );
+    }
+  }
+
+  .service('navService', [
+          '$q',
+          navService
+  ]);
+
+  function navService($q){
+    var menuItems = [
+      {
+        name: 'Dashboard',
+        icon: 'dashboard',
+        sref: '.dashboard'
+      },
+      {
+        name: 'Profile',
+        icon: 'person',
+        sref: '.profile'
+      },
+      {
+        name: 'Table',
+        icon: 'view_module',
+        sref: '.table'
+      }
+    ];
+
+    return {
+      loadAllItems : function() {
+        return $q.when(menuItems);
+      }
+    };
+  }
